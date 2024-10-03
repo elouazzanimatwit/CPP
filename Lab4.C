f33018784f256a1b0144fe9ad83cd47a5cc66964
Mouncef <elouazzanim@wit.edu>
2024-10-03 10:06:46 -0400

    ddd

--- Lab4.C
+++ Lab4.C
@@ -10,29 +10,28 @@ int main(){
     mypid = getpid();
     printf("Process ID before fork is %d \n", mypid);
     pid = fork();
-
+    mypid = getpid();
     if(pid == -1){
         printf("Fork Failure!");
         exit(pid);
     }
 
     if(pid == 0){
-        mypid = getpid();
+        
         printf("Child process id is %d and Parent process id is %d \n", pid, mypid);
         printf("Any command after this point will not be printed. \n");
 
-        int status_code=execvp("ls", arg_list);
+        int status_code=execvp("date", arg_list);
 
         if(status_code == -1){
-            printf("If you see this, then execvp() hasn't terminated properly");
+            perror("execvp() failed");
+            exit(1);
         }
     } else {
         int status;
         waitpid(pid, &status, 0);
-        printf("Parent process id is %d \n", pid);
+        printf("Parent process id is %d \n", mypid);
         printf("Parent's child has exited with status %d \n", status);
-
-
         printf("My child exit code is %d\n", WIFEXITED(status));
     }
 
